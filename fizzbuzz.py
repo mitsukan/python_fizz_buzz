7c71befd7dbffdc85bbfd5c08f9bb0ff30fc6730
Jihin Ip <jim_ip20@hotmail.com>
2018-07-08 20:15:19 +0100

    Modifying 3 eq fizz test: fizz divisible by 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,5 @@
 def run(x):
-    if x == 3:
+    if x % 3 == 0:
         return "Fizz!"
     elif x == 5:
         return "Buzz!"
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,8 +3,9 @@ import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
 
-    def test_3_eq_fizz(self):
+    def test_fizz_divisible_by_3(self):
         self.assertEqual(fizzbuzz.run(3), "Fizz!")
+        self.assertEqual(fizzbuzz.run(6), "Fizz!")
 
     def test_5_eq_buzz(self):
         self.assertEqual(fizzbuzz.run(5), "Buzz!")
