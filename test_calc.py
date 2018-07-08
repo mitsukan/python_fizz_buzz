f4f7e48ac1ff6b98b74d398054d6a084da41b6be
Jihin Ip <jim_ip20@hotmail.com>
2018-07-08 19:24:28 +0100

    Testing and passing calc.subract

--- test_calc.py
+++ test_calc.py
@@ -6,6 +6,8 @@ class TestCalc(unittest.TestCase): # test class inherits from unittest.TestCase
     def test_add(self): # all tests need to start with test_ as a naming convention
         self.assertEqual(calc.add(10,5), 15)
 
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(10,5), 5)
 
 # To run the test, we need to enter in the terminal:
 # $ python -m unittest test_calc.py
