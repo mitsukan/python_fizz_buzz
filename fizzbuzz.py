6ca356ba8057ce93b510888866184a2ae85252f4
Jihin Ip <jim_ip20@hotmail.com>
2018-07-08 20:18:50 +0100

    Passing test for fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def run(x):
-    if x % 3 == 0:
-        return "Fizz!"
+    if x % 15 == 0:
+        return "FizzBuzz!"
     elif x % 5 == 0:
         return "Buzz!"
+    elif x % 3 == 0:
+        return "Fizz!"
