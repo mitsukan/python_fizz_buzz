bf6d49ceb430ebb70062f7eff7a2cdf9aff8500e
Jihin Ip <jim_ip20@hotmail.com>
2018-07-09 19:28:19 +0100

    Adding a test for the Roshambo class to have an array of choices

--- rock_paper_scissors/test_RPS.py
+++ rock_paper_scissors/test_RPS.py
@@ -7,3 +7,8 @@ class TestRoshambo(unittest.TestCase):
         """User should be able to input their choice"""
         new_game = Roshambo("rock")
         self.assertEqual(new_game.input, "rock")
+
+    def test_choices(self):
+        """Game has an array to store the possible choices"""
+        new_game = Roshambo("rock")
+        self.assertEqual(new_game.choices, ["rock", "paper", "scissors"])
