4ad41ac401801a54d40f764b09a112c9097b6143
Jihin Ip <jim_ip20@hotmail.com>
2018-07-29 14:36:59 +0100

    Passing tests for 3 5 15 in fizzbuzz pytest. Adding test for returning number if divisible by nothing

--- pycharm_fizzbuzz/fizzbuzz.py
+++ pycharm_fizzbuzz/fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(x):
-    if x % 3 == 0:
+    if x % 15 == 0:
+        return "fizzbuzz"
+    elif x % 3 == 0:
         return "fizz"
-    else:
+    elif x % 5 == 0:
         return "buzz"
--- pycharm_fizzbuzz/test_fizzbuzz.py
+++ pycharm_fizzbuzz/test_fizzbuzz.py
@@ -2,6 +2,14 @@ from fizzbuzz import fizzbuzz
 
 def test_fizz_divisible_by_3():
     assert fizzbuzz(3) == "fizz"
+    assert fizzbuzz(6) == "fizz"
 
 def test_buzz_divisible_by_5():
     assert fizzbuzz(5) == "buzz"
+    assert fizzbuzz(10) == "buzz"
+
+def test_fizzbuzz_divisible_by_15():
+    assert fizzbuzz(15) == "fizzbuzz"
+
+def test_return_x_if_divisible_by_nothing():
+    assert fizzbuzz(2) == 2
