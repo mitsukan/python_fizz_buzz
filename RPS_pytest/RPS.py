f0bbd9a7f32868c2efed0e4f87efc7789cf750c8
Jihin Ip <jim_ip20@hotmail.com>
2018-07-29 15:36:31 +0100

    Adding and passing tests for rock beating scissors, win condition for both p1 and p2

--- RPS_pytest/RPS.py
+++ RPS_pytest/RPS.py
@@ -1,2 +1,5 @@
 def roshambo(p1,p2):
-    pass
+    if (p1 == "rock" and p2 == "scissors"):
+        return "Player 1 wins!"
+    elif (p2 == "rock" and p1 == "scissors"):
+        return "Player 2 wins!"
