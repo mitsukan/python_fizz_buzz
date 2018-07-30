97035d1391525884842cb2bd6d6cb52b7215b6cc
Jihin Ip <jihinip@Jihins-MacBook-Pro.local>
2018-07-30 13:50:52 +0100

    Changing tests from pytest to unittest, adding new test for 2nd win condition

--- RPS_pytest/test_RPS.py
+++ RPS_pytest/test_RPS.py
@@ -1,5 +1,14 @@
-from RPS import roshambo
+import unittest
+import RPS
 
-def test_rock_beats_scissors():
-    assert roshambo("rock","scissors") == "Player 1 wins!"
-    assert roshambo("scissors", "rock") == "Player 2 wins!"
+class TestRPS(unittest.TestCase):
+
+    def test_rock_beats_scissors(self):
+        self.assertEqual(RPS.roshambo("rock", "scissors"), "Player 1 wins!")
+        self.assertEqual(RPS.roshambo("scissors", "rock"), "Player 2 wins!")
+
+    def test_scissors_beats_paper(self):
+        self.assertEqual(RPS.roshambo("scissors", "paper"), "Player 1 wins!")
+
+if __name__ == '__main__':
+    unittest.main()
