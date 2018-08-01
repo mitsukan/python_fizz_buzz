167b211fa1b13cb5fd9f3921023b921894e92ef0
Jihin Ip <jim_ip20@hotmail.com>
2018-08-01 12:54:40 +0100

    Adding and passing test for add method in Menu

--- takeaway_py/menu.py
+++ takeaway_py/menu.py
@@ -3,3 +3,6 @@ class Menu(object):
 
     def __init__(self):
         self.items = {}
+
+    def add(self, item, price):
+        self.items[item] = price
--- takeaway_py/test_menu.py
+++ takeaway_py/test_menu.py
@@ -8,6 +8,11 @@ class TestMenu(unittest.TestCase):
         m = menu.Menu()
         self.assertIsInstance(m.items, dict)
 
+    def test_can_add_item_to_menu(self):
+        m = menu.Menu()
+        m.add("chips", 1.7)
+        self.assertEqual(m.items, {"chips": 1.7})
+
 
 if __name__ == '__main__':
     unittest.main()
