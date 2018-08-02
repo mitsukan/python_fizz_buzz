706bfaccdedaf42d40e161fc7b6ec07d87068853
Jihin Ip <jim_ip20@hotmail.com>
2018-08-02 11:31:46 +0100

    Amending test and code for add method, to accept quantity

--- takeaway_py/POS.py
+++ takeaway_py/POS.py
@@ -6,8 +6,11 @@ class Till(object):
         self.order = []
         self.menu = Menu()
 
-    def add(self, item):
-        self.order.append(item)
+    def add(self, item, quantity):
+        i = 1
+        while i <= quantity:
+            self.order.append(item)
+            i += 1
 
     def total(self):
         running_total = 0
--- takeaway_py/test_POS.py
+++ takeaway_py/test_POS.py
@@ -11,17 +11,17 @@ class TestPOS(unittest.TestCase):
 
     def test_can_receive_an_order(self):
         pos = POS.Till()
-        pos.add("chips")
-        pos.add("sausage")
-        self.assertEqual(pos.order, ["chips", "sausage"])
+        pos.add("chips", 3)
+        pos.add("sausage", 2)
+        self.assertEqual(pos.order, ["chips", "chips", "chips", "sausage", "sausage"])
 
 
     def test_can_total_up_the_order_using_menu_prices(self):
         with mock.patch('POS.Menu') as mock_menu:
             mock_menu().items = {"item1": 1.0, "item2": 2.0}
             pos = POS.Till()
-            pos.add("item1")
-            pos.add("item2")
+            pos.add("item1", 1)
+            pos.add("item2", 1)
             self.assertEqual(pos.total(), 3.0)
 
 
